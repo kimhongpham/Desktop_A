11f8b7428f0762e266108ebfc685985f3d18f06f
hnhngc <huynhngc176@gmail.com>
2024-02-16 23:50:37 +0700

    Memory Game

---
+++ MEMORY_MATCH/MEMORY MATCH/MEMORY MATCH/MainGame.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace MEMORY_MATCH
+{
+    partial class MainGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_level = new Guna.UI2.WinForms.Guna2Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel_score = new Guna.UI2.WinForms.Guna2Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel_times = new Guna.UI2.WinForms.Guna2Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2PictureBox6 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2PictureBox5 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2PictureBox4 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2PictureBox3 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2PictureBox2 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.btn_setting = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.btn_pause = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.panel_level.SuspendLayout();
+            this.panel_score.SuspendLayout();
+            this.panel_times.SuspendLayout();
+            this.guna2Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel_level
+            // 
+            this.panel_level.BackColor = System.Drawing.Color.Transparent;
+            this.panel_level.BorderColor = System.Drawing.Color.Black;
+            this.panel_level.BorderRadius = 20;
+            this.panel_level.BorderThickness = 2;
+            this.panel_level.Controls.Add(this.label1);
+            this.panel_level.Location = new System.Drawing.Point(12, 12);
+            this.panel_level.Name = "panel_level";
+            this.panel_level.Size = new System.Drawing.Size(80, 80);
+            this.panel_level.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Level";
+            // 
+            // panel_score
+            // 
+            this.panel_score.BackColor = System.Drawing.Color.Transparent;
+            this.panel_score.BorderColor = System.Drawing.Color.Black;
+            this.panel_score.BorderRadius = 19;
+            this.panel_score.BorderThickness = 2;
+            this.panel_score.Controls.Add(this.label2);
+            this.panel_score.Location = new System.Drawing.Point(98, 12);
+            this.panel_score.Name = "panel_score";
+            this.panel_score.Size = new System.Drawing.Size(200, 40);
+            this.panel_score.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Score";
+            // 
+            // panel_times
+            // 
+            this.panel_times.BackColor = System.Drawing.Color.Transparent;
+            this.panel_times.BorderColor = System.Drawing.Color.Black;
+            this.panel_times.BorderRadius = 19;
+            this.panel_times.BorderThickness = 2;
+            this.panel_times.Controls.Add(this.label3);
+            this.panel_times.Location = new System.Drawing.Point(98, 55);
+            this.panel_times.Name = "panel_times";
+            this.panel_times.Size = new System.Drawing.Size(168, 40);
+            this.panel_times.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 32);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Times";
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Panel1.Controls.Add(this.guna2PictureBox6);
+            this.guna2Panel1.Controls.Add(this.guna2PictureBox5);
+            this.guna2Panel1.Controls.Add(this.guna2PictureBox4);
+            this.guna2Panel1.Controls.Add(this.guna2PictureBox3);
+            this.guna2Panel1.Controls.Add(this.guna2PictureBox2);
+            this.guna2Panel1.Controls.Add(this.guna2PictureBox1);
+            this.guna2Panel1.Location = new System.Drawing.Point(21, 113);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(767, 325);
+            this.guna2Panel1.TabIndex = 4;
+            // 
+            // guna2PictureBox6
+            // 
+            this.guna2PictureBox6.BorderRadius = 2;
+            this.guna2PictureBox6.ImageRotate = 0F;
+            this.guna2PictureBox6.Location = new System.Drawing.Point(480, 160);
+            this.guna2PictureBox6.Name = "guna2PictureBox6";
+            this.guna2PictureBox6.Size = new System.Drawing.Size(100, 100);
+            this.guna2PictureBox6.TabIndex = 5;
+            this.guna2PictureBox6.TabStop = false;
+            // 
+            // guna2PictureBox5
+            // 
+            this.guna2PictureBox5.ImageRotate = 0F;
+            this.guna2PictureBox5.Location = new System.Drawing.Point(310, 160);
+            this.guna2PictureBox5.Name = "guna2PictureBox5";
+            this.guna2PictureBox5.Size = new System.Drawing.Size(100, 100);
+            this.guna2PictureBox5.TabIndex = 4;
+            this.guna2PictureBox5.TabStop = false;
+            // 
+            // guna2PictureBox4
+            // 
+            this.guna2PictureBox4.ImageRotate = 0F;
+            this.guna2PictureBox4.Location = new System.Drawing.Point(145, 160);
+            this.guna2PictureBox4.Name = "guna2PictureBox4";
+            this.guna2PictureBox4.Size = new System.Drawing.Size(100, 100);
+            this.guna2PictureBox4.TabIndex = 3;
+            this.guna2PictureBox4.TabStop = false;
+            // 
+            // guna2PictureBox3
+            // 
+            this.guna2PictureBox3.ImageRotate = 0F;
+            this.guna2PictureBox3.Location = new System.Drawing.Point(480, 24);
+            this.guna2PictureBox3.Name = "guna2PictureBox3";
+            this.guna2PictureBox3.Size = new System.Drawing.Size(100, 100);
+            this.guna2PictureBox3.TabIndex = 2;
+            this.guna2PictureBox3.TabStop = false;
+            // 
+            // guna2PictureBox2
+            // 
+            this.guna2PictureBox2.ImageRotate = 0F;
+            this.guna2PictureBox2.Location = new System.Drawing.Point(310, 24);
+            this.guna2PictureBox2.Name = "guna2PictureBox2";
+            this.guna2PictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.guna2PictureBox2.TabIndex = 1;
+            this.guna2PictureBox2.TabStop = false;
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.ImageRotate = 0F;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(145, 24);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.guna2PictureBox1.TabIndex = 0;
+            this.guna2PictureBox1.TabStop = false;
+            // 
+            // btn_setting
+            // 
+            this.btn_setting.BackColor = System.Drawing.Color.Transparent;
+            this.btn_setting.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.btn_setting.HoverState.ImageSize = new System.Drawing.Size(64, 64);
+            this.btn_setting.Image = global::MEMORY_MATCH.Properties.Resources.setting;
+            this.btn_setting.ImageOffset = new System.Drawing.Point(0, 0);
+            this.btn_setting.ImageRotate = 0F;
+            this.btn_setting.ImageSize = new System.Drawing.Size(50, 50);
+            this.btn_setting.Location = new System.Drawing.Point(731, 7);
+            this.btn_setting.Name = "btn_setting";
+            this.btn_setting.PressedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.btn_setting.Size = new System.Drawing.Size(57, 63);
+            this.btn_setting.TabIndex = 5;
+            // 
+            // btn_pause
+            // 
+            this.btn_pause.BackColor = System.Drawing.Color.Transparent;
+            this.btn_pause.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.btn_pause.HoverState.ImageSize = new System.Drawing.Size(64, 64);
+            this.btn_pause.Image = global::MEMORY_MATCH.Properties.Resources.pause;
+            this.btn_pause.ImageOffset = new System.Drawing.Point(0, 0);
+            this.btn_pause.ImageRotate = 0F;
+            this.btn_pause.ImageSize = new System.Drawing.Size(50, 50);
+            this.btn_pause.Location = new System.Drawing.Point(669, 12);
+            this.btn_pause.Name = "btn_pause";
+            this.btn_pause.PressedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.btn_pause.Size = new System.Drawing.Size(56, 56);
+            this.btn_pause.TabIndex = 6;
+            // 
+            // MainGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OldLace;
+            this.BackgroundImage = global::MEMORY_MATCH.Properties.Resources.hinhnen;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_pause);
+            this.Controls.Add(this.btn_setting);
+            this.Controls.Add(this.guna2Panel1);
+            this.Controls.Add(this.panel_times);
+            this.Controls.Add(this.panel_score);
+            this.Controls.Add(this.panel_level);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainGame";
+            this.panel_level.ResumeLayout(false);
+            this.panel_level.PerformLayout();
+            this.panel_score.ResumeLayout(false);
+            this.panel_score.PerformLayout();
+            this.panel_times.ResumeLayout(false);
+            this.panel_times.PerformLayout();
+            this.guna2Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel panel_level;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2Panel panel_score;
+        private System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2Panel panel_times;
+        private System.Windows.Forms.Label label3;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox6;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox5;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox4;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox3;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox2;
+        private Guna.UI2.WinForms.Guna2ImageButton btn_setting;
+        private Guna.UI2.WinForms.Guna2ImageButton btn_pause;
+    }
+}
